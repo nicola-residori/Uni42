ad00b1f36c29676d8dd367562172c8ee478c10e0
Nicola Residori <42891965+nicola-residori@users.noreply.github.com>
2023-01-18 20:53:19 +0100

    added JWT token to swagger on channelApi

--- ChannelAPI/src/main/java/com/uni42/channelapi/config/SwaggerConfig.java
+++ ChannelAPI/src/main/java/com/uni42/channelapi/config/SwaggerConfig.java
@@ -5,12 +5,17 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -22,7 +27,9 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.basePackage("com.uni42.channelapi.controller"))
                 .paths(PathSelectors.any())
                 .build()
-                .apiInfo(apiInfo());
+                .apiInfo(apiInfo())
+                .securityContexts(Collections.singletonList(securityContext()))
+                .securitySchemes(Collections.singletonList(apiKey()));
     }
 
     private ApiInfo apiInfo() {
@@ -33,6 +40,21 @@ public class SwaggerConfig {
                 "Terms of service",
                 new Contact("Uni42", "www.uni42.com", "info@uni42.com"),
                 "License of API", "API license URL", Collections.emptyList());
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Collections.singletonList(new SecurityReference("JWT", authorizationScopes));
     }
 
 }
